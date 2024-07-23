a353f875ee7426e391209b97cf9ce5f26a3047c0
Antonio Marques <antonio@quimbik.com>
2024-07-23 16:27:02 -0300

    Create User Model

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+# email: string
+# password_digest: string
+#
+# password: string (virtual attribute)
+# password_confirmation: string (virtual attribute)
+
+
+class User < ApplicationRecord
+    has_secure_password
+end
